32e221878e7247c58d0072763acd9cb4cff730e3
Louis CarricoBraun <lcarrico-braun@wlapcs.org>
2017-03-23 18:40:38 +0000

    even better

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var x = 300;
 var y = 200;
-var speedx = 8;
-var speedy = 6;
+var speedx = 6;
+var speedy = 4;
+var img;
 
+function preload() {
+  img = loadImage("budi.png");
+}
 
 function setup(){
     createCanvas(600,400);
@@ -11,22 +15,34 @@ function setup(){
 function draw(){
     background(0);
 
+    display();
+
+    move();
+    
+    bounce();
+    
+}
+
+function display(){
     //display ball
     stroke(255);
     strokeWeight(4);
     fill(50,250,50);
-    ellipse(x, y, 50, 50);
+    image(img, x, y, 100, 100);
+}
 
-    
+function move(){
     //make ball move
     y = y + speedy;
     x = x + speedx;
-    
+}
+
+function bounce(){
     //ball changes direction when it hits wall
-    if(x + 25 >600 || x - 25 < 0){
+    if(x + 100 >600 || x < 0){
         speedx = -speedx;
     }
-     if(y + 25 > 400 || y - 25 < 0){
+     if(y + 100 > 400 || y < 0){
         speedy = -speedy;
     }
 }
